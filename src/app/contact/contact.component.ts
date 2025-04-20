d92d70d5ba33297931cbdd15653cb40db7018a93
arturito <58474213+arthures11@users.noreply.github.com>
2025-04-20 23:16:47 +0200

    initial fix

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -17,16 +17,27 @@ export class ContactComponent implements OnInit{
         this.copiedMail = false
     }
 
-    async copyToClipboard(value: string) {
+    async copyToClipboardDiscord(value: string) {
         try {
             // @ts-ignore
             await navigator.clipboard.writeText(value)
             if(value === 'arthvro') this.copiedDiscord = true
+
+            setTimeout(() => {
+                this.copiedDiscord = false
+            }, 2000);
+        } catch (err) {
+            console.error('Failed to copy!', err);
+        }
+    }
+    async copyToClipboardMail(value: string) {
+        try {
+            // @ts-ignore
+            await navigator.clipboard.writeText(value)
             if(value === 'artbryja@gmail.com') this.copiedMail = true
 
             setTimeout(() => {
                 this.copiedMail = false
-                this.copiedDiscord = false
             }, 2000);
         } catch (err) {
             console.error('Failed to copy!', err);
